421cd34e0867740bf0e3fde82d5c9233f51abf1b
Mattia46 <mattia.assogna@gmail.com>
2015-12-17 15:40:48 +0000

    added the basic button and first func to insert the city name to search

--- RespApiCall-weather/RespApiCall-weather/ViewController.swift
+++ RespApiCall-weather/RespApiCall-weather/ViewController.swift
@@ -10,6 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var DescriptionLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    @IBAction func setCityButton(sender: UIButton) {
+//        print("ciao")
+        openCityAlert()
+    }
+    
+    func openCityAlert(){
+        
+        let alert = UIAlertController(title: "City",
+            message: "Enter city name",
+            preferredStyle: UIAlertControllerStyle.Alert)
+        
+        let cancel = UIAlertAction(title: "Calcel",
+            style: UIAlertActionStyle.Cancel,
+            handler: nil)
+        
+        alert.addAction(cancel)
+        
+        let ok = UIAlertAction(title: "OK",
+            style: UIAlertActionStyle.Default) { (action: UIAlertAction) -> Void in
+                print("OK")
+        }
+        
+        alert.addAction(ok)
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
