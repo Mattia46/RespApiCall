a50264393a9394aa3c71cef117e3bacfc2955715
Mattia46 <mattia.assogna@gmail.com>
2015-12-17 17:10:54 +0000

    add the request func with a fake API

--- RespApiCall-weather/RespApiCall-weather/WeatherService.swift
+++ RespApiCall-weather/RespApiCall-weather/WeatherService.swift
@@ -20,13 +20,24 @@ class WeatherService {
     
     
     func getWeather(city: String) {
-        print("Weather Service city: \(city)")
         
-        let weather = Weather(cityName: city, temp: 237.12, description: "A nice day")
-        
-        if delegate != nil {
-           delegate?.setWeather(weather)
+        let path = "http://api.openweathermap.org/data/2.5/weather?q=Pescara,appid=2de143494c0b295cca9337e1e96b00e0"
+        let url = NSURL(string: path)
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithURL(url!) { (data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
+            print(">>>>>\(data)")
+            print(response)
         }
+        task.resume()
+        
+        
+//        print("Weather Service city: \(city)")
+//        
+//        let weather = Weather(cityName: city, temp: 237.12, description: "A nice day")
+//        
+//        if delegate != nil {
+//           delegate?.setWeather(weather)
+//        }
         
     }
     
